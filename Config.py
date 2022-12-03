e3e080331d09705c94d0e012ac75f4b5ca0a5a21
gestaltix-com-au <49353748+gestaltix-com-au@users.noreply.github.com>
2022-12-03 21:22:11 +1100

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,3 @@
+source = 'http://<ip of your local envoy>/production.json'
+endPoint = 'https://api.chargehq.net/api/public/push-solar-data>'
+apiKey = '<your apiKey from https://app.chargehq.net/config/energy-monitor>'
